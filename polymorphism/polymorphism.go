e358b321321789abb1d25ed475cfe346fbd45293
yernur <yernur.abishev@gmail.com>
2022-12-14 18:40:07 +0600

    example

--- polymorphism/polymorphism.go
+++ polymorphism/polymorphism.go
@@ -1,4 +1,35 @@
 package main
 
+import "fmt"
+
+type Vehicle interface {
+	move()
+}
+
+type Car struct {
+	model string
+}
+
+type Aircraft struct {
+	model string
+}
+
+func (c Car) move() {
+	fmt.Println(c.model, "driving")
+}
+
+func (a Aircraft) move() {
+	fmt.Println(a.model, "flying")
+}
+
 func main() {
+	tesla := Car{"Tesla"}
+	volvo := Car{"Volvo"}
+	boeing := Aircraft{"Boeing"}
+
+	vehicles := [...]Vehicle{tesla, volvo, boeing}
+
+	for _, values := range vehicles {
+		values.move()
+	}
 }
